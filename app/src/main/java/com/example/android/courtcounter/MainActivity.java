d5df636adbafa03e397d2dd0ae615bc52e1650e4
vikaskumar.s <vikaskumar.s@zohocorp.com>
2018-10-20 14:19:59 +0530

    court counter first push

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.android.courtcounter;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0,scoreTeamB = 0;
+
+    ScoreViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
+
+        displayScoreTeamA( viewModel.getScoreTeamA());
+        displayScoreTeamB( viewModel.getScoreTeamB());
+
+    }
+
+    void displayScoreTeamA(int score){
+        TextView scoreViewTeamA  = (TextView) findViewById(R.id.score_team_a);
+        scoreViewTeamA.setText(""+score);
+
+    }
+
+    void displayScoreTeamB(int score){
+        TextView scoreViewTeamB  = (TextView) findViewById(R.id.score_team_b);
+        scoreViewTeamB.setText(""+score);
+
+    }
+    public void threePointsTeamA(View view){
+            scoreTeamA = scoreTeamA + 3;
+            viewModel.setScoreTeamA(scoreTeamA);
+        displayScoreTeamA(viewModel.getScoreTeamA());
+    }
+    public void twoPointsTeamA(View view){
+        scoreTeamA = scoreTeamA + 2;
+        viewModel.setScoreTeamA(scoreTeamA);
+        displayScoreTeamA(viewModel.getScoreTeamA());
+    }
+    public void onePointsTeamA(View view){
+        scoreTeamA = scoreTeamA + 1;
+        viewModel.setScoreTeamA(scoreTeamA);
+        displayScoreTeamA(viewModel.getScoreTeamA());
+    }
+    public void threePointsTeamB(View view){
+        scoreTeamB = scoreTeamB + 3;
+        viewModel.setScoreTeamB(scoreTeamB);
+        displayScoreTeamB(viewModel.getScoreTeamB());
+    }
+    public void twoPointsTeamB(View view){
+        scoreTeamB = scoreTeamB + 2;
+        viewModel.setScoreTeamB(scoreTeamB);
+        displayScoreTeamB(viewModel.getScoreTeamB());
+    }
+    public void onePointsTeamB(View view){
+        scoreTeamB = scoreTeamB + 1;
+        viewModel.setScoreTeamB(scoreTeamB);
+        displayScoreTeamB(viewModel.getScoreTeamB());
+    }
+    public void resetScore(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        viewModel.setScoreTeamA(0);
+        viewModel.setScoreTeamB(0);
+        displayScoreTeamA(viewModel.getScoreTeamA());
+        displayScoreTeamB(viewModel.getScoreTeamB());
+    }
+
+    public void check_score(View view) {
+        Toast.makeText(MainActivity.this,"Team A Score: "+scoreTeamA,Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/android/courtcounter/ScoreViewModel.java
@@ -0,0 +1,29 @@
+package com.example.android.courtcounter;
+
+
+import android.arch.lifecycle.ViewModel;
+
+public class ScoreViewModel extends ViewModel {
+
+    //Track Score Team A
+    private int scoreTeamA = 0;
+
+    //Track Score Team B
+    private int scoreTeamB = 0;
+
+    public int getScoreTeamA() {
+        return scoreTeamA;
+    }
+
+    public int getScoreTeamB() {
+        return scoreTeamB;
+    }
+
+    public void setScoreTeamA(int scoreTeamA) {
+        this.scoreTeamA = scoreTeamA;
+    }
+
+    public void setScoreTeamB(int scoreTeamB) {
+        this.scoreTeamB = scoreTeamB;
+    }
+}
