c340b7b9cb0b626e3a433fe4d103491f684396d0
vikaskumar.s <vikaskumar.s@zohocorp.com>
2019-11-06 22:08:25 +0530

    Exploring RecyclerView Example added.

--- app/src/main/java/com/codewithvikas/exploring/android/MainActivity.java
+++ app/src/main/java/com/codewithvikas/exploring/android/MainActivity.java
@@ -10,33 +10,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<UserInput> userInputs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        userInputs = new ArrayList<>();
-
-        updateUserInput();
-
-
-    }
-
-    private void updateUserInput() {
-        userInputs.add(new UserInput("mohan",2));
-        userInputs.add(new UserInput("ram",4));
-        userInputs.add(new UserInput("ramesh",6));
-        userInputs.add(new UserInput("ali",9));
-        userInputs.add(new UserInput("sardar",78));
-        userInputs.add(new UserInput("pankaj",9));
-        userInputs.add(new UserInput("roshan",20));
     }
 
     public void openRecyclerViewActivity(View view) {
-        Intent intent = new Intent(this, RecyclerViewActivity.class);
-
-        intent.putParcelableArrayListExtra("userInputs",userInputs);
-        startActivity(intent);
+        startActivity(new Intent(this,RecyclerViewActivity.class));
     }
 }
