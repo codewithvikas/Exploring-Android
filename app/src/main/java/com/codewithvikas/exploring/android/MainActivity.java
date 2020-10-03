994697f849890a4f563659f144adf1e5e1dc1e98
Vikas Jha <vikasjhag@gmail.com>
2020-10-03 20:18:24 +0530

    migrated to AndroidX. Updated latest framework library.

--- app/src/main/java/com/codewithvikas/exploring/android/MainActivity.java
+++ app/src/main/java/com/codewithvikas/exploring/android/MainActivity.java
@@ -1,12 +1,7 @@
 package com.codewithvikas.exploring.android;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
