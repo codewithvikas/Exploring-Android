303080fbea611ddb819210f6a4daa115556225eb
Vikas Jha <vikasjhag@gmail.com>
2020-10-04 10:26:13 +0530

    ExerciseListAdapter added.

--- app/src/main/java/com/codewithvikas/exploring/android/ExerciseListAdapter.java
+++ app/src/main/java/com/codewithvikas/exploring/android/ExerciseListAdapter.java
@@ -1,7 +1,9 @@
 package com.codewithvikas.exploring.android;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,19 +13,30 @@ import java.util.ArrayList;
 public class ExerciseListAdapter extends RecyclerView.Adapter<ExerciseListAdapter.ExerciseViewHolder> {
 
     ArrayList<String> mExerciseList;
-    ExerciseListAdapter(ArrayList<String> exerciseNameList){
-        mExerciseList = exerciseNameList;
+    ExerciseListAdapterOnClickHandler mExerciseListAdapterOnClickHandler;
+    ExerciseListAdapter(ExerciseListAdapterOnClickHandler exerciseListAdapterOnClickHandler){
+        mExerciseListAdapterOnClickHandler = exerciseListAdapterOnClickHandler;
     }
 
+    interface ExerciseListAdapterOnClickHandler{
+        void onExerciseClickHandler(String exercise);
+    }
     @NonNull
     @Override
     public ExerciseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.exercise_list_item,parent,false);
+        return new ExerciseViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ExerciseViewHolder holder, int position) {
+            holder.bindView(mExerciseList.get(position));
+    }
 
+    public void setExerciseList(ArrayList<String> mExerciseList) {
+        this.mExerciseList = mExerciseList;
+        notifyDataSetChanged();
     }
 
     @Override
@@ -31,10 +44,24 @@ public class ExerciseListAdapter extends RecyclerView.Adapter<ExerciseListAdapte
         return mExerciseList.size();
     }
 
-    static class ExerciseViewHolder extends RecyclerView.ViewHolder{
 
+     class ExerciseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView exerciseTv;
         public ExerciseViewHolder(@NonNull View itemView) {
             super(itemView);
+            exerciseTv = itemView.findViewById(R.id.exercise_tv);
+            itemView.setOnClickListener(this);
+        }
+
+        void bindView(String exercise){
+            exerciseTv.setText(exercise);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            mExerciseListAdapterOnClickHandler.onExerciseClickHandler(mExerciseList.get(position));
+
         }
     }
 }
--- app/src/main/java/com/codewithvikas/exploring/android/MainActivity.java
+++ app/src/main/java/com/codewithvikas/exploring/android/MainActivity.java
@@ -1,15 +1,51 @@
 package com.codewithvikas.exploring.android;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements ExerciseListAdapter.ExerciseListAdapterOnClickHandler{
+
+    Toast mToast;
+    ExerciseListAdapter mExerciseListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        RecyclerView recyclerView = findViewById(R.id.exercise_list_rv);
 
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+         mExerciseListAdapter = new ExerciseListAdapter(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(mExerciseListAdapter);
+
+        initExerciseList();
+
+    }
+        void initExerciseList(){
+            ArrayList<String> exerciseList = new ArrayList<>();
+            exerciseList.add("RecyclerView");
+            exerciseList.add("Simple Java");
+            exerciseList.add("Passing Data Via Intent");
+
+            mExerciseListAdapter.setExerciseList(exerciseList);
+
+        }
+    @Override
+    public void onExerciseClickHandler(String exercise) {
+        if (mToast!=null){
+            mToast.cancel();
+        }
+        mToast = Toast.makeText(this,"Exercise:"+exercise+" clicked",Toast.LENGTH_SHORT);
+        mToast.show();
+    }
 }
